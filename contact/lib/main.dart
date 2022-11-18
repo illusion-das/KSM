d166d220467a3b0086793e8edd826a01e185575e
tjddnd0 <116141179+tjddnd0@users.noreply.github.com>
2022-11-18 09:26:53 +0900

    11/17

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -1,7 +1,8 @@
-import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
-import 'package:animated_splash_screen/animated_splash_screen.dart';
 
+import 'package:flutter/services.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:transition/transition.dart';
 
 
 void main() {
@@ -18,6 +19,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        pageTransitionsTheme: PageTransitionsTheme(
+          builders: {
+            TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(),
+            TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
+          },
+        ),
+      ),
       debugShowCheckedModeBanner: false,
       home: AnimatedSplashScreen(
         splash: Image.asset('assets/asmlogore.png'),
@@ -48,21 +57,34 @@ class Route1 extends StatelessWidget {
             Row(
               children: [
                 Container(
+<<<<<<< HEAD
+                  width: 75,
+                  height: 70,
                   padding: EdgeInsets.only(left:10),
+                  child: Image.asset('assets/asmlogore.png', width: 90,),
+=======
+                  padding: EdgeInsets.only(left: 10),
                   child: Image.asset('assets/asmlogore.png', width: 75,),
+>>>>>>> baae437f56c049d43c12a71ca075f15b27753598
                 ),
               ],
             ),
           Container(
           height: 140,
+            padding: EdgeInsets.only(top:10),
           margin: EdgeInsets.fromLTRB(15, 0, 15, 15),
             decoration: BoxDecoration(
               color: Color(0xff0d6eb8),
               borderRadius: BorderRadius.circular(22),
               boxShadow: [
                 BoxShadow(
+<<<<<<< HEAD
                   color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 1,
+=======
+                  color: Colors.grey.withOpacity(0.75),
                   spreadRadius: 2,
+>>>>>>> baae437f56c049d43c12a71ca075f15b27753598
                   blurRadius: 7,
                   offset: Offset(0, 3), // changes position of shadow
                 ),
@@ -81,7 +103,7 @@ class Route1 extends StatelessWidget {
                     boxShadow: [
                       BoxShadow(
                         color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 2,
+                        spreadRadius: 1,
                         blurRadius: 7,
                         offset: Offset(0, 3), // changes position of shadow
                       ),
@@ -98,7 +120,7 @@ class Route1 extends StatelessWidget {
                     boxShadow: [
                       BoxShadow(
                         color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 2,
+                        spreadRadius: 1,
                         blurRadius: 7,
                         offset: Offset(0, 3), // changes position of shadow
                       ),
@@ -110,9 +132,161 @@ class Route1 extends StatelessWidget {
             Row(
               children: [
                 Container(
-                  margin: EdgeInsets.fromLTRB(15, 0, 15, 15),
-                    child: Text('유용한 기능')
+                  margin: EdgeInsets.fromLTRB(25, 5, 15, 15),
+                    child: Text('유용한 기능', style:TextStyle(
+                        fontFamily:'Pretendard',fontSize:30,
+                        fontWeight:FontWeight.bold,
+                        color: Color(0xff4c4c4d)),
+                    )
                 ),
+              ],
+            ),
+            Column(
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      width: 80,
+                      height: 80,
+                      margin: EdgeInsets.fromLTRB(18, 0, 3, 15),
+                      decoration: BoxDecoration(
+                        color: Color(0xffffffff),
+                        borderRadius: BorderRadius.circular(22),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 1,
+                            blurRadius: 7,
+                            offset: Offset(0, 3), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: 80,
+                      height: 80,
+                      margin: EdgeInsets.fromLTRB(15, 0, 15, 15),
+                      decoration: BoxDecoration(
+                        color: Color(0xffffffff),
+                        borderRadius: BorderRadius.circular(22),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 1,
+                            blurRadius: 7,
+                            offset: Offset(0, 3), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: 80,
+                      height: 80,
+                      margin: EdgeInsets.fromLTRB(3, 0, 3, 15),
+                      decoration: BoxDecoration(
+                        color: Color(0xffffffff),
+                        borderRadius: BorderRadius.circular(22),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 1,
+                            blurRadius: 7,
+                            offset: Offset(0, 3), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: 80,
+                      height: 80,
+                      margin: EdgeInsets.fromLTRB(15, 0, 15, 15),
+                      decoration: BoxDecoration(
+                        color: Color(0xffffffff),
+                        borderRadius: BorderRadius.circular(22),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 1,
+                            blurRadius: 7,
+                            offset: Offset(0, 3), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Container(
+                      width: 80,
+                      height: 80,
+                      margin: EdgeInsets.fromLTRB(18, 0, 3, 15),
+                      decoration: BoxDecoration(
+                        color: Color(0xffffffff),
+                        borderRadius: BorderRadius.circular(22),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 1,
+                            blurRadius: 7,
+                            offset: Offset(0, 3), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: 80,
+                      height: 80,
+                      margin: EdgeInsets.fromLTRB(15, 0, 15, 15),
+                      decoration: BoxDecoration(
+                        color: Color(0xffffffff),
+                        borderRadius: BorderRadius.circular(22),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 1,
+                            blurRadius: 7,
+                            offset: Offset(0, 3), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: 80,
+                      height: 80,
+                      margin: EdgeInsets.fromLTRB(3, 0, 3, 15),
+                      decoration: BoxDecoration(
+                        color: Color(0xffffffff),
+                        borderRadius: BorderRadius.circular(22),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 1,
+                            blurRadius: 7,
+                            offset: Offset(0, 3), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: 80,
+                      height: 80,
+                      margin: EdgeInsets.fromLTRB(15, 0, 15, 15),
+                      decoration: BoxDecoration(
+                        color: Color(0xffffffff),
+                        borderRadius: BorderRadius.circular(22),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 1,
+                            blurRadius: 7,
+                            offset: Offset(0, 3), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                )
               ],
             )
           ],
@@ -128,26 +302,19 @@ class Route1 extends StatelessWidget {
             ),
             IconButton(
               icon: Icon(Icons.food_bank_rounded),
-              onPressed: () {
-                Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => Route2()),
-              );
-                },
+              onPressed: () {},
             ),
             IconButton(
               icon: Icon(Icons.money_rounded),
-              onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => Route3()),
-              );},
+              onPressed: () {},
             ),
             IconButton(
               icon: Icon(Icons.school_rounded),
-              onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => Route4()),
-              );},
+              onPressed: () {},
             ),
             IconButton(
               icon: Icon(Icons.tag_faces_rounded),
-              onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => Route5()),
-              );},
+              onPressed: () {},
             ),
           ],
         )
@@ -155,239 +322,6 @@ class Route1 extends StatelessWidget {
     );
   }
 }
-
-class Route2 extends StatelessWidget {
-  const Route2({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xfff8f8f9),
-        shadowColor: Colors.transparent,
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context, MaterialPageRoute(builder: (context) => Route1()),
-            );
-          },
-          child: Text('급식'),
-        ),
-      ),
-      backgroundColor: Color(0xfff8f8f9),
-      bottomNavigationBar: BottomAppBar(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              IconButton(
-                icon: Icon(Icons.home_rounded),
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => Route1()),
-                  );
-                },
-              ),
-              IconButton(
-                icon: Icon(Icons.food_bank_rounded),
-                onPressed: () {},
-              ),
-              IconButton(
-                icon: Icon(Icons.money_rounded),
-                onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => Route3()),
-                );},
-              ),
-              IconButton(
-                icon: Icon(Icons.school_rounded),
-                onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => Route4()),
-                );},
-              ),
-              IconButton(
-                icon: Icon(Icons.tag_faces_rounded),
-                onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => Route5()),
-                );},
-              ),
-            ],
-          )
-      ),
-    );
-  }
-}
-
-class Route3 extends StatelessWidget {
-  const Route3({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xfff8f8f9),
-        shadowColor: Colors.transparent,
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('포인트'),
-        ),
-      ),
-      backgroundColor: Color(0xfff8f8f9),
-      bottomNavigationBar: BottomAppBar(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              IconButton(
-                icon: Icon(Icons.home_rounded),
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => Route1()),
-                  );
-                },
-              ),
-              IconButton(
-                icon: Icon(Icons.food_bank_rounded),
-                onPressed: () {Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Route2()),
-                );},
-              ),
-              IconButton(
-                icon: Icon(Icons.money_rounded),
-                onPressed: () {},
-              ),
-              IconButton(
-                icon: Icon(Icons.school_rounded),
-                onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => Route4()),
-                );},
-              ),
-              IconButton(
-                icon: Icon(Icons.tag_faces_rounded),
-                onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => Route5()),
-                );},
-              ),
-            ],
-          )
-      ),
-    );
-  }
-}
-
-class Route4 extends StatelessWidget {
-  const Route4({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xfff8f8f9),
-        shadowColor: Colors.transparent,
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('시간표'),
-        ),
-      ),
-      backgroundColor: Color(0xfff8f8f9),
-      bottomNavigationBar: BottomAppBar(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              IconButton(
-                icon: Icon(Icons.home_rounded),
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => Route1()),
-                  );
-                },
-              ),
-              IconButton(
-                icon: Icon(Icons.food_bank_rounded),
-                onPressed: () {Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Route2()),
-                );},
-              ),
-              IconButton(
-                icon: Icon(Icons.money_rounded),
-                onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => Route3()),
-                );},
-              ),
-              IconButton(
-                icon: Icon(Icons.school_rounded),
-                onPressed: () {},
-              ),
-              IconButton(
-                icon: Icon(Icons.tag_faces_rounded),
-                onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => Route5()),
-                );},
-              ),
-            ],
-          )
-      ),
-    );
-  }
-}
-
-class Route5 extends StatelessWidget {
-  const Route5({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xfff8f8f9),
-        shadowColor: Colors.transparent,
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('프로필'),
-        ),
-      ),
-      backgroundColor: Color(0xfff8f8f9),
-      bottomNavigationBar: BottomAppBar(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              IconButton(
-                icon: Icon(Icons.home_rounded),
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => Route1()),
-                  );
-                },
-              ),
-              IconButton(
-                icon: Icon(Icons.food_bank_rounded),
-                onPressed: () {Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Route2()),
-                );},
-              ),
-              IconButton(
-                icon: Icon(Icons.money_rounded),
-                onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => Route3()),
-                );},
-              ),
-              IconButton(
-                icon: Icon(Icons.school_rounded),
-                onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => Route4()),
-                );},
-              ),
-              IconButton(
-                icon: Icon(Icons.tag_faces_rounded),
-                onPressed: () {},
-              ),
-            ],
-          )
-      ),
-    );
-  }
-}
-
-
 
 
 /*ElevatedButton(
